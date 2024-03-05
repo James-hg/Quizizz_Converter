837569aa2c22134cf4b55c724830bec750e09c32
James Hoang <jameshoang2304@gmail.com>
2024-03-05 17:24:10 +0700

    hello

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-// function uploadFile() {
-//     const fileInput = document.getElementById('fileInput');
-//     const file = fileInput.files[0];
+function uploadFile() {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
 
-//     if (!file) {
-//         alert('Please select a file.');
-//         return;
-//     }
+  if (!file) {
+    alert("Please select a file.");
+    return;
+  }
 
-//     const formData = new FormData();
-//     formData.append('file', file);
+  const formData = new FormData();
+  formData.append("file", file);
 
-//     fetch('/upload', {
-//         method: 'POST',
-//         body: formData
-//     })
-//     .then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//         throw new Error('File upload failed.');
-//     })
-//     .then(data => {
-//         alert(data.message);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//         alert('Error uploading file.');
-//     });
-// }
-
-for (let i = 0; i < 50; i++) {
-  console.log("hello");
+  fetch("/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("File upload failed.");
+    })
+    .then((data) => {
+      alert(data.message);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Error uploading file.");
+    });
 }
